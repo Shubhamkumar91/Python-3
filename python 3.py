4327c4cd3cbfc294f7b259d499e1895a8d9101b1
Shubham kumar <118390347+Shubhamkumar91@users.noreply.github.com>
2022-12-06 19:42:06 +0530

    Add files via upload

---
+++ python 3.py
@@ -0,0 +1,52 @@
+'''Your task is to find the name of the student with maximum marks after updation in marks and the jump in the student’s rank i.e., previous rank – current rank.
+
+You are given three lists’ names, mark’s and update’s where:
+
+1-Names contain the names of students.
+2-Marks contain the marks of the same students.
+3-Updates contains the integers by which the marks of these - - students are to be updated.'''
+# Python3 implementation of the approach
+
+# Function to print the name of student who
+# stood first after updation in rank
+def nameRank(names, marks, updates, n):
+	
+	# Array of students
+	x = [[0 for j in range(3)] for i in range(n)]
+	for i in range(n):
+		
+		# Store the name of the student
+		x[i][0] = names[i]
+		
+		# Update the marks of the student
+		x[i][1]= marks[i] + updates[i]
+		
+		# Store the current rank of the student
+		x[i][2] = i + 1
+		
+	highest = x[0]
+	for j in range(1, n):
+		if (x[j][1] >= highest[1]):
+			highest = x[j]
+			
+	# Print the name and jump in rank
+	print("Name: ", highest[0], ", Jump: ",
+			abs(highest[2] - 1), sep="")
+
+# Driver code
+
+# Names of the students
+names= ["Student1", "Student2", "Student3"]
+
+# Marks of the students
+marks = [80, 79, 75]
+
+# Updates that are to be done
+updates = [0, 5, -9]
+
+# Number of students
+n = len(marks)
+
+nameRank(names, marks, updates, n)
+
+# This code is contributed by SHUBHAMSINGH10
